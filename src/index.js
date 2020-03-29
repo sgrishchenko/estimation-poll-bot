1a26359055fc564ed4934f24caa2eda65071f39e
sgrishchenko <sgrishchenko@spotware.com>
2020-03-30 00:59:53 +0300

    implement poll via telegraf

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const axios = require('axios');
 const Telegraf = require('telegraf');
 
 const BOT_TOKEN = process.env.BOT_TOKEN || "861074090:AAGwj5x6Hto9q1izTW-n_1O_rrTUToL6BDc";
@@ -9,7 +6,23 @@ const URL = process.env.URL || "https://estimation-poll-bot.herokuapp.com/";
 
 const bot = new Telegraf(BOT_TOKEN);
 
-bot.command('newestimate', (ctx) => ctx.reply('Here will be a poll.'));
+bot.command('newestimate', (ctx) => {
+    ctx.replyWithPoll('Estimation', [
+        '1 story point',
+        '2 story points',
+        '3 story points',
+        '5 story points',
+        '8 story points',
+        '13 story points',
+        '21 story points',
+    ], {
+        is_anonymous: false,
+    });
+});
+
+bot.command('help', (ctx) => {
+    return ctx.reply('This bot will help you create polls for task estimation. Send /newestimate to create a new estimation poll.');
+});
 
 bot.telegram.setWebhook(`${URL}/bot`)
     .then(() => {
@@ -19,30 +32,3 @@ bot.telegram.setWebhook(`${URL}/bot`)
     .then(() =>{
         console.log('Estimation Poll Bot is started!');
     });
-
-// axios
-//     .post(
-//         'https://api.telegram.org/bot861074090:AAGwj5x6Hto9q1izTW-n_1O_rrTUToL6BDc/sendPoll',
-//         {
-//             chat_id: message.chat.id,
-//             is_anonymous: false,
-//             question: 'Estimation',
-//             options: [
-//                 '1 story point',
-//                 '2 story points',
-//                 '3 story points',
-//                 '5 story points',
-//                 '8 story points',
-//                 '13 story points',
-//                 '21 story points',
-//             ]
-//         }
-//     )
-//     .then(() => {
-//         console.log('Poll posted');
-//         res.end('ok')
-//     })
-//     .catch(err => {
-//         console.log('Error: ', err);
-//         res.end('Error: ' + err)
-//     })
