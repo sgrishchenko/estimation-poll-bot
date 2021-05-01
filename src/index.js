9fdd9f75df0c03773cac6547051aa5f0c6c57c52
Sergey Grishchenko <s.i.grishchenko@gmail.com>
2021-05-02 00:31:31 +0300

    libs are updated, http endpoint is added, special option for showing of results is added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-import Telegraf from 'telegraf';
+import 'dotenv/config.js';
+import {createServer} from 'http';
+import {Telegraf} from 'telegraf';
 
-const BOT_TOKEN = process.env.BOT_TOKEN;
-const PORT = process.env.PORT || 3000;
-const URL = process.env.URL || 'https://estimation-poll-bot.herokuapp.com/';
+const NODE_ENV = process.env.NODE_ENV ?? 'development';
+const PORT = Number(process.env.PORT) ?? 3000;
+
+const BOT_URL = process.env.BOT_URL ?? '';
+const BOT_TOKEN = process.env.BOT_TOKEN ?? '';
 
 const bot = new Telegraf(BOT_TOKEN);
 
 bot.command('newestimate', (ctx) => {
     ctx.replyWithPoll('Estimation', [
+        'Show results',
         '1 story point',
         '2 story points',
         '3 story points',
@@ -24,11 +29,31 @@ bot.command('help', (ctx) => {
     return ctx.reply('This bot will help you create polls for task estimation. Send /newestimate to create a new estimation poll.');
 });
 
-bot.telegram.setWebhook(`${URL}/bot`)
-    .then(() => {
-        bot.startWebhook('/bot', null, PORT);
-        return bot.launch();
-    })
-    .then(() =>{
+const requestListener = (request, response) => {
+    if (request.url === '/') {
+        bot.telegram.getWebhookInfo().then((webhookInfo) => {
+            const result = {
+                telegramIsHooked: webhookInfo.url.includes(BOT_URL),
+            };
+
+            response.statusCode = 200;
+            response.setHeader('Content-Type', 'application/json');
+            response.end(JSON.stringify(result, null, 2));
+        });
+    } else {
+        bot.webhookCallback(`/bot${BOT_TOKEN}`)(request, response);
+    }
+};
+
+if (NODE_ENV === 'production') {
+    bot.telegram.setWebhook(`${BOT_URL}/bot${BOT_TOKEN}`).then(() => {
+        const server = createServer(requestListener);
+        server.listen(PORT, () => {
+            console.log('Estimation Poll Bot is started!');
+        })
+    });
+} else {
+    bot.launch().then(() => {
         console.log('Estimation Poll Bot is started!');
     });
+}
